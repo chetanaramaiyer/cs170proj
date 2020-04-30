b9113673942004cbeb2248e9e65a52aac9997b6c
Deepali Jain <deepali.jain.sd@gmail.com>
2020-04-30 14:49:01 -0700

    IT WORKS on our input graphs yay

--- solver.py
+++ solver.py
@@ -13,35 +13,31 @@ def solve(G):
         T: networkx.Graph
     """
 
-    # TODO: your code here!
     vertices = list(G.nodes)
     edges = list(G.edges.data('weight'))
-    res = KruskalMST(vertices, edges)
+    res = KruskalMST(vertices, edges) #creates mst
 
     T = nx.Graph()
     T.add_weighted_edges_from(res)
-    avg_dist = average_pairwise_distance_fast(T)
+    avg_dist = average_pairwise_distance_fast(T) #current min pairwise avg of mst
 
     leaves = getLeaves(res)
-    prunedEdges, prunedVertices = pruneLeavesDesc(leaves, res, avg_dist)
-    T.remove_edges_from(prunedEdges)
-    T.remove_nodes_from(prunedVertices)
-    print(T.edges.data('weight'))
-    print(T.nodes)
-    
+    prunedEdges, prunedVertices, avgRand = pruneLeavesRando(leaves, res, avg_dist)
+    prunedEdgesD, prunedVerticesD, avgDesc = pruneLeavesDesc(leaves, res, avg_dist)
+    #chooses better pairwise avg between descending and randomized leaves
+    if avgDesc < avgRand:
+    	T.remove_edges_from(prunedEdgesD)
+    	T.remove_nodes_from(prunedVerticesD)
+    else:
+    	T.remove_edges_from(prunedEdges)
+    	T.remove_nodes_from(prunedVertices)
+
     return T
 
-# Python program for Kruskal's algorithm to find 
-# Minimum Spanning Tree of a given connected, 
-# undirected and weighted graph 
-
 def sortEdges(tup):
-	#print(edges)  
-
-    # getting length of list of tuples 
-    lst = len(tup)  
-    for i in range(0, lst):
-          
+	# getting length of list of tuples 
+    lst = len(tup)
+    for i in range(0, lst):  
         for j in range(0, lst-i-1):  
             if (tup[j][2] > tup[j + 1][2]):  
                 temp = tup[j]  
@@ -50,7 +46,12 @@ def sortEdges(tup):
     return tup
 
 def KruskalMST(vertices, edges): 
-
+	'''
+		input:
+			vertices: vertices of our input graph G in a list
+			edges: edges of G in a list
+		output: a list of edges of our resultant MST
+	'''
 	result = [] #This will store the resultant MST 
 
 	i = 0 # An index variable, used for sorted edges 
@@ -89,7 +90,7 @@ def KruskalMST(vertices, edges):
 			result.append((u,v,w)) 
 			union(parent, rank, x, y)			 
 		# Else discard the edge 
-	print(result)
+	#print(result)
 	return result
 	# print the contents of result[] to display the built MST 
 	#print "Following are the edges in the constructed MST"
@@ -125,7 +126,8 @@ def union(parent, rank, x, y):
 def getLeaves(mst):
 	'''
 		input: mst graph
-		output: 
+		output: a dictionary of leaves
+			   {v : (u,v,w), ...} where v is a leaf with its corresponding edge
 	'''
 	nodes = {}
 	for u,v,w in mst:
@@ -139,20 +141,31 @@ def getLeaves(mst):
 			nodes[v] = (u,v,w)
 
 	nodes = {x:y for x,y in nodes.items() if y!=(-1,-1,-1)}
-	print(nodes)
+	#print(nodes)
 	return nodes
 
 def pruneLeavesDesc(l, res, currentAvg):
-	#VERY naive solution bc we repeat avg_pairwise_distance method on EVERY call
-	leaves = l.values()
-	#print(leaves)
+	'''
+		input: l = all leaves of mst
+			   res = complete mst
+			   avg = mst's current avg 
+		output: (best edges, best vertices) of leaves to be removed
+				from mst to minimize avg
+				runs in order from largest edge to lowest edge
+	'''
+	leaves = l
 	removedLeaves = []
 	temp = res.copy()
-	verticesRemoved = {}
+	verticesRemoved = []
+
 	#start with the largest edge weight of leaves first
-	for elem in reverse(leaves):
+	def by_value(item):
+		return item[1]
+
+	for vertex, elem in sorted(leaves.items(), key=by_value, reverse=True):
+		i = temp.index(elem) #index of curr elem; needed so we can add back to temp in right order
 		temp.remove(elem)
-		print(temp)
+		#print(temp)
 		#create new graph to recalculate avg pairwise distance w/o elem
 		Tr = nx.Graph()
 		Tr.add_weighted_edges_from(temp)
@@ -161,50 +174,67 @@ def pruneLeavesDesc(l, res, currentAvg):
 		#else, add it back and move on to next leaf
 		if new_avg <= currentAvg:
 			removedLeaves.append(elem)
-			verticesRemoved.add([elem[0], elem[1]]) #add vertices to this set
+			verticesRemoved.append(vertex) #add vertices to this set
 			currentAvg = new_avg
-			print("removed:"+ str(new_avg))
 		else:
-			temp.add(elem)
-			print("kept:"+ str(new_avg))
+			temp.insert(i, elem)
+	print("descAvg:" + str(currentAvg))
+	return (removedLeaves, verticesRemoved, currentAvg)
 
-	return (removedLeaves, verticesRemoved)
-
-def pruneLeavesRando(l, currentAvg, res):
-	#randomize the order and see if we get different solutions?
-	leaves = sortEdges(l)
-	print(leaves)
+def pruneLeavesRando(l, res, avg):
+	'''
+		input: l = all leaves of mst
+			   res = complete mst
+			   avg = mst's current avg 
+		output: (best edges, best vertices) of leaves to be removed
+				from mst to minimize avg
+	'''
 	removedLeaves = []
 	temp = res.copy()
+	currentAvg = avg
 	bestAvg = currentAvg
 	bestLeaves = []
+	verticesRemoved = []
+	bestVerticesRemoved = []
+	keys = list(l.keys())
+	def by_value(item):
+		return item[1]
 	#start with the largest edge weight of leaves first
 	for i in range(0, 15):
-		leaves = random.shuffle(leaves)
-		for elem in leaves:
+		#shuffle dictionary
+		random.shuffle(keys)
+		leaves = dict()
+		for key in keys:
+  			leaves.update({key:l[key]})
+
+		for vertex, elem in leaves.items():
+			ind = temp.index(elem)
 			temp.remove(elem)
-			print(temp)
+
 			#create new graph to recalculate avg pairwise distance w/o elem
 			Tr = nx.Graph()
 			Tr.add_weighted_edges_from(temp)
-			new_avg = average_pairwise_distance(Tr)
+			new_avg = average_pairwise_distance_fast(Tr)
 			#if better avg obtained w/o elem: remove it and update avg
 			#else, add it back and move on to next leaf
 			if new_avg <= currentAvg:
 				removedLeaves.append(elem)
+				verticesRemoved.append(vertex) #add vertices to this set
 				currentAvg = new_avg
-				print("removed:"+ str(new_avg))
 			else:
-				temp.add(elem)
-				print("kept:"+ str(new_avg))
+				temp.insert(i, elem)
 		if new_avg <= bestAvg:
 			bestLeaves = removedLeaves.copy()
 			bestAvg = new_avg
-			print(new_avg + "    " + "iteration " +  i)
+			bestVerticesRemoved = verticesRemoved.copy()
+			#print(str(new_avg) + "    " + "iteration " +  str(i))
+
 		removedLeaves = []
-
-
-	return bestLeaves
+		verticesRemoved = []
+		currentAvg = avg
+		temp = res.copy()
+	print("rando" + str(bestAvg))
+	return (bestLeaves, bestVerticesRemoved, bestAvg)
 
 
 
